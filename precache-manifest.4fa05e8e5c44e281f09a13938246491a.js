bbcf3d7603e488b48bbe42434bc43e463aa0e384
justMG <bob.vsauce.xt2@gmail.com>
2025-03-26 20:19:17 +0100

    Updates

---
+++ precache-manifest.4fa05e8e5c44e281f09a13938246491a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "975d2fde63d2f848028570c4123715bc",
+    "url": "/wrocnow/index.html"
+  },
+  {
+    "revision": "7525ab4e2846bb7a21d3",
+    "url": "/wrocnow/static/css/main.18d7f7c1.chunk.css"
+  },
+  {
+    "revision": "91532fe74c9ded204142",
+    "url": "/wrocnow/static/js/2.a7f37d0f.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/wrocnow/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "7525ab4e2846bb7a21d3",
+    "url": "/wrocnow/static/js/main.5c4135d2.chunk.js"
+  },
+  {
+    "revision": "4baaa1b53342bb013200",
+    "url": "/wrocnow/static/js/runtime~main.1ccbb700.js"
+  },
+  {
+    "revision": "2c9cf932e147a2a6661ad6c823329ce9",
+    "url": "/wrocnow/static/media/logo.2c9cf932.png"
+  }
+]);
